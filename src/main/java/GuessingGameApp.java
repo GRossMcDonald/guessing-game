3e9c175d0998d9c5071fb912a37820537488351d
GRossMcDonald <ross_mcdonald@outlook.com>
2024-05-21 18:28:13 -0400

    Guessing Game

--- src/main/java/GuessingGameApp.java
+++ src/main/java/GuessingGameApp.java
@@ -7,51 +7,54 @@ public class GuessingGameApp {
         Scanner userInput = new Scanner(System.in);
         Random random = new Random();
         int randomNumber = random.nextInt(10) + 1;
-        int attempts = 0;
-        
-        //Just for testing purposes.
+        int attempts = 1;
+
+        // Just for testing purposes.
         System.out.println("Random Number: " + randomNumber);
 
-        //Instruction print outs.
+        // Instruction print outs.
         System.out.println("Welcome to the Guessing Game!");
         System.out.println("You will be given three attempts to guess a random number (1-10) correctly.");
         System.out.println("If you guess the number correctly, you win the game.");
         System.out.println("Input '0' to receive instructions again.");
         System.out.println("Input '-1' to exit the game.");
-        
-        while (/*something*/) {
+
+        while (attempts <= 3) {
             System.out.println("Please input a number between 1 and 10.");
-        int guess = userInput.nextInt();
-        System.out.println("User input: " + guess);
-        if (guess < -1 || guess > 10) {
-            System.out.println("Remember to guess a number between 1 and 10");
+            int guess = userInput.nextInt();
+            System.out.println("User input: " + guess);
+            if (guess < -1 || guess > 10) {
+                System.out.println("Remember to guess a number between 1 and 10");
+                continue;
+            }
+            if (guess == 0) {
+                System.out.println("You will be given three attempts to guess a random number (1-10) correctly.");
+                System.out.println("If you guess the number correctly, you win the game.");
+                System.out.println("Input '0' to receive instructions again.");
+                System.out.println("Input '-1' to exit the game.");
+                continue;
+            }
+            if (guess == -1) {
+                System.out.println("You have exited the game. Goodbye.");
+                break;
+            }
+            if (guess == randomNumber) {
+                System.out.println("That's the correct guess! You win!");
+                break;
+            }
+            if (guess > randomNumber) {
+                System.out.println("Try guessing a lower number!");
+                attempts++;
+            } else {
+                System.out.println("Try guessing a higher number!");
+                attempts++;
+            }
+            
         }
-        if (guess == 0) {
-            System.out.println("You will be given three attempts to guess a random number (1-10) correctly.");
-            System.out.println("If you guess the number correctly, you win the game.");
-            System.out.println("Input '0' to receive instructions again.");
-            System.out.println("Input '-1' to exit the game.");
-            //need the opposite of a break statement or something
-        }
-        if (guess == -1) {
-            System.out.println("You have exited the game. Goodbye.");
-            break;
-        }
-        if (guess == randomNumber) {
-            System.out.println("That's the correct guess! You win!");
-            break;
-        }
-        if (guess > randomNumber) {
-            System.out.println("Try guessing a lower number!");
-        } else {
-            System.out.println("Try guessing a higher number!");
-        }
-        // attempts incremented may need to be in a different place.
-        attempts++;
-        if (attempts >= 3) {
+        if (attempts > 3) {
             System.out.println("Sorry you have run out of guesses!");
             System.out.println("The correct number was " + randomNumber);
         }
-        }
+        userInput.close();
     }
 }
